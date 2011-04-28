b635fc997a18bedc4f8e22e6c45dc31103eeeaf2
Tim Reilly <tim.f.reilly@gmail.com>
2011-04-28 02:05:50 +0000

    Updated gameEnd to use its member variables instead of parameters.  Hopefully in line with future code goals.
    git-svn-id: https://tools-n-candy.svn.beanstalkapp.com/unlucky-at-python/trunk@32 e074218b-c350-4508-b1a8-6d95c45357c9

--- src/menu.py
+++ src/menu.py
@@ -145,7 +145,6 @@ def menuChoice(offense,defense):
     elif turnAction=="DIG":
         offense.dig+=10
         return 1
-    
     elif turnAction=="MENU":
         print "A high Bravery means better punches, but you must up close."
         print "If you Grab the banker, your punches are more likely to hit."
@@ -228,31 +227,27 @@ class Scenario:
         self.npc.addWeapon()
         time.sleep(1)
 
-    def gameEnd(self,offense,defense):
+    def gameEnd(self):
         print
         print
-        if defense.wound<=-20:
-            print defense.cap_name, "has sustained serious wounds and can't stop you from \n grabbing the cash.  You win!"
-            return True
-        elif offense.intimcount==3:
-            print "You've intimidated",defense.name,"into submission and make off with the cash!"
-            return True
-        elif defense.concuss<=-15:
-            print defense.cap_name,"has sustained a concussion and sinks to the floor. You reach over him and grab the cash!"
-            return True
-        elif defense.intimcount==3:
-            print defense.cap_name,"is far too intimidating and will never back down.  You lose!"
-            return True
-        elif offense.wound<=-20:
+        if self.npc.wound<=-20:
+            print self.npc.defense.cap_name, "has sustained serious wounds and can't stop you from \n grabbing the cash.  You win!"
+        elif self.player.intimcount==3:
+            print "You've intimidated",self.npc.name,"into submission and make off with the cash!"
+        elif self.npc.defense.concuss<=-15:
+            print self.npc.cap_name,"has sustained a concussion and sinks to the floor. You reach over him and grab the cash!"
+        elif self.npc.intimcount==3:
+            print self.npc.cap_name,"is far too intimidating and will never back down.  You lose!"
+        elif self.player.wound<=-20:
             print "You have sustained serious wounds.  You lose!"
-            return True
         elif self.roundCount>10:
             print "You've waited too long and the sheriff walks in the door.  You lose!"
-            return True
         elif self.quitChoice==True:
             return True
-        else:
+        else: #if it doesn't match any of the possible game ending conditions, the game has not ended
             return False
+        #if any of the above was true, return that the game is over
+        return True 
 
     def rollInitiative(self):
         #does not handle if people roll the an identical place
@@ -275,7 +270,7 @@ class Scenario:
         self.quitChoice=False
         self.roundCount=1
         turnCount = 0
-        while self.gameEnd(self.player,self.npc)==False:  #TODO: Should test thoroughly that game can end mid-turn
+        while not self.gameEnd():  #TODO: Should test thoroughly that game can end mid-turn
             if not turnCount:
                 print "~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~"
                 print "Beginning round number ",self.roundCount,"."
