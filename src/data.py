25b14ec2e8a733ab677b62b69afb4778d55f059b
Tim Reilly <tim.f.reilly@gmail.com>
2011-05-05 08:14:23 +0000

    adding a data definition file
    git-svn-id: https://tools-n-candy.svn.beanstalkapp.com/unlucky-at-python/trunk@82 e074218b-c350-4508-b1a8-6d95c45357c9

---
+++ src/data.py
@@ -0,0 +1,4 @@
+allWeapons=[{'name':'six-shooter',  'range':1,  'maxbullets':6,   'sharp':0,    'isRange':True},
+            {'name':'shotgun',      'range':2,  'maxbullets':3,   'sharp':10,   'isRange':True},
+            {'name':'rifle',        'range':0,  'maxbullets':1,   'sharp':30,   'isRange':True},
+            {'name':'saber',        'range':0,  'maxbullets':10,  'sharp':20,   'isRange':False}]
