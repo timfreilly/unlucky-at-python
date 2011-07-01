cda4c9bb9b7a2772ec5926361ba99d2bf71d94e9
Tim Reilly <tim.f.reilly@gmail.com>
2011-07-01 01:37:24 +0000

    Tightened up showGuide.
    git-svn-id: https://tools-n-candy.svn.beanstalkapp.com/unlucky-at-python/trunk@155 e074218b-c350-4508-b1a8-6d95c45357c9

--- src/menu.py
+++ src/menu.py
@@ -73,13 +73,12 @@ class Battle:
         
     def showGuide(self):
         self.turnOver = False
-        print "A high Bravery means better punches.  Grabbing also helps melee strikes land."
-        print "Melee hits cause concussion damage, and each point of concussion makes\n all actions 1% less likely to succeed."
+        print "High Bravery or a successful grab aids melee strikes."
+        print "Melee hits aren't as sharp as bullets, but concussion does add up."
+        print "Your accuracy with a gun relies on your Concentration."        
         print "Intimidation relies on your Grit. If you win the battle of wills you push your opponent closer to cowering in fear."
         print "Digging Deep improves any action on your next turn by 20%."
-        print "If you draw on the same turn you fire your gun, your are 15% less likely to hit."
-        print "If you draw and wait until your next turn, your shot is 20% more likely to hit."
-        print "Your accuracy with a gun relies on your Concentration."
+        print "If you draw on the same turn you fire your gun, you are less likely to hit."
         print
         print
         time.sleep(2)
